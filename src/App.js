91bb0476033b2752f6cd31602834fe74faeb9864
almamunhossen <almamunhossen@gmail.com>
2024-04-10 23:38:33 +0300

    new updat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let names = [
+    { name: "Jayan Hossen", father: "Al Mamun Hossen" },
+    { name: "Ayan Hossen", father: "Al Mamun Hossen" },
+    { name: "Al Mamun Hossen", father: "Zafar Hossain" },
+    { name: "Yamen Munna", father:"Jammal Uddain" },
+  ];
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +15,33 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        {
+          names.map(name =>
+            <Person name={name.name} father={name.father} address="Dhaka, Bangladesh"></Person>
+          )
+        }
       </header>
     </div>
   );
+}
+
+function Person(x) {
+  const divStyle = {
+    backgroundColor: "red",
+    width: "230px",
+    borderRadius: "8px",
+    margin: '10px',
+    lineHeight: "5px",
+  }
+  return (
+    <div style={divStyle}>
+      <h3>{x.name}</h3>
+      <h5>{x.father}</h5>
+      <p>{x.address}</p>
+    </div>
+  )
+
 }
 
 export default App;
